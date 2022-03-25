c23d6d72e8ea4ab7d7f38fa41814b7ae4a7b658d
RICHARD HENRIQUE COUTO DA ROCHA <richard.hcrocha@senacsp.edu.br>
2022-03-24 22:08:10 -0300

    criar funcoes no component e executá-las, interpolando com {}

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+
+  function handleButtonClick(){
+    alert('Clickou no botão')
+  }
+
+  function sum(a,b){
+    return a + b;
+  }
   return (
     <>
       <Header tittle ="Meu Primeiro Component React"/>
       <Header tittle ="Um Component com Props"/>
       <h2>
         Senac Campinas
+        <div>Retorno da Soma:{sum(5,6)}</div>
       </h2>
+      <button onClick={handleButtonClick}>Click Me!</button>
     </>
   );
 }
