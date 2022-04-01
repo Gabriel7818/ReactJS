ea7840b5507ba83519a3b5ec02ab0a7310e9237c
GABRIEL OLIVEIRA DAMASCENO <gabriel.odamasceno@senacsp.edu.br>
2022-03-31 22:01:49 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,52 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './Header';
 
 function App() {
 
   function handleButtonClick(){
-    alert('Clickou no botão')
+    alert('Clicou no botão')
   }
 
   function sum(a,b){
     return a + b;
   }
+
+  // Conceito de estado do component
+  // Sempre que eu quiser algo na tela esteja reagindo ao
+  // Comportamento da minha aplicação;
+  // Isto é a base do React...
+  // Reagir ao comportamento, eu tenho que criar um estado ou state
+
+  const [contador, setContador ]= useState(0);
+
+  // A função useState vai criar um state e vai retornar a variavel
+  // Que vai retornar o estado(contador) e a função que vai alterar
+  // Estado (setContador)
+  // Sempre que Eu quiser mudar o valor e notificar o Frontend,
+  // Chamo a função que vai alterar a variável...
+  // Que vai ser devolvida para o Frontend
+  // (Esta é a comunicação do State com o Frontend)
+
+  // let contador = 0;
+  function handleButtonContar(){
+    // Contador = contador + 1;
+    // contador++;
+    setContador(contador+1);
+  }
+
   return (
-    <>
-      <Header tittle ="Meu Primeiro Component React"/>
-      <Header tittle ="Um Component com Props"/>
+    <div className="App">
+      <Header title ="Meu Primeiro Component React"/>
+      <Header title ="Um Component com Props"/>
       <h2>
         Senac Campinas
         <div>Retorno da Soma:{sum(5,6)}</div>
       </h2>
       <button onClick={handleButtonClick}>Click Me!</button>
-    </>
+      <Header title={contador}/>
+      <button onClick={handleButtonContar}>Contar</button>
+    </div>
   );
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props) {
   return(
     <header>
-         <h1>{props.tittle}</h1>
+         <h1>{props.title}</h1>
     </header>
   )
 }
